feebba5e729e697df9d8c12b032020b2a47ac924
Rory Salvatore <rory121212@gmail.com>
2021-12-25 23:32:37 +0200

    feat: react version 17

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,7 @@
 import {
   configure, shallow, render, mount,
 } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import React from 'react';
 
 // Make Enzyme functions available in all test files without importing
